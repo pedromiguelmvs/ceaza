f2f82b2228c730341787ac67e46464913382881b
pedromiguelmvs <pedromiguelmvs@gmail.com>
2021-11-29 12:58:25 -0300

    feature: inicia página de equipe e adiciona correções em projetos

--- js/projetos.js
+++ js/projetos.js
@@ -3,91 +3,110 @@ const formElement = document.querySelector("form[name=form]");
 const sectionCardsElement = document.querySelector("ul[class=cards]");
 
 const inputTitleElement = document.querySelector("input[name=title]");
-const inputDescriptionElement = document.querySelector("input[name=description]");
+const inputDescriptionElement = document.querySelector(
+  "input[name=description]"
+);
 const emptyElement = document.querySelector("section[class=empty]");
-
-const articleElement = document.querySelectorAll("article[class=card]");
 
 const projects = [];
 
 if (!projects.length) {
-    sectionCardsElement.style['height'] = '300px';
-    sectionCardsElement.style['display'] = 'flex';
-    sectionCardsElement.style['align-items'] = 'center';
-    sectionCardsElement.style['justify-content'] = 'center';
-    emptyElement.style['display'] = 'flex';
+  sectionCardsElement.style["height"] = "300px";
+  sectionCardsElement.style["display"] = "flex";
+  sectionCardsElement.style["align-items"] = "center";
+  sectionCardsElement.style["justify-content"] = "center";
+  emptyElement.style["display"] = "flex";
 }
 
 function listProjects() {
-    const articles = document.querySelectorAll("li");
-    
-    for (const item of articles) {
-        sectionCardsElement.removeChild(item);
-    }
+  const articles = document.querySelectorAll("li");
 
-    for (const item of projects) {
-        const li = document.createElement("li");
-        const card = document.createElement("div");
+  for (const item of articles) {
+    sectionCardsElement.removeChild(item);
+  }
 
-        const title = document.createElement("h2");
-        const titleValue = document.createTextNode(item.title);
-        title.appendChild(titleValue);
+  let counter = -1;
 
-        const description = document.createElement("p");
-        const descriptionValue = document.createTextNode(item.title);
-        description.appendChild(descriptionValue);
+  for (const item of projects) {
+    const li = document.createElement("li");
+    const card = document.createElement("div");
 
-        const img = document.createElement('img');
-        img.src = '../assets/close.svg';
-        img.alt = 'deletar';
-        
-        card.appendChild(title);
-        card.appendChild(description);
-        li.appendChild(card);
-        li.appendChild(img);
-        li.className = 'card';
+    const title = document.createElement("h2");
+    const titleValue = document.createTextNode(item.title);
+    title.appendChild(titleValue);
 
-        sectionCardsElement.appendChild(li);
-    }
+    const description = document.createElement("p");
+    const descriptionValue = document.createTextNode(item.description);
+    description.appendChild(descriptionValue);
+
+    const img = document.createElement("img");
+    img.src = "../assets/close.svg";
+    img.alt = "deletar";
+
+    li.value = ++counter;
+
+    card.appendChild(title);
+    card.appendChild(description);
+    li.appendChild(card);
+    li.appendChild(img);
+    li.className = "card";
+
+    sectionCardsElement.appendChild(li);
+  }
 }
 
 function showForm() {
-    if (formElement.style['display'] === 'flex') {
-        return formElement.style['display'] = 'none'
-    }
+  if (formElement.style["display"] === "flex") {
+    return (formElement.style["display"] = "none");
+  }
 
-    formElement.style['display'] = 'flex';
+  formElement.style["display"] = "flex";
 }
 
-buttonCreateElement.addEventListener('click', showForm);
+buttonCreateElement.addEventListener("click", showForm);
 
 function onCreate(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const formData = new FormData(formElement);
+  const formData = new FormData(formElement);
 
-    if (!formData.get('title') || !formData.get('description')) {
-        return false;
-    }
+  if (!formData.get("title") || !formData.get("description")) {
+    return false;
+  }
 
-    const title = formData.get('title');
-    const description = formData.get('description');
+  const title = formData.get("title");
+  const description = formData.get("description");
 
-    if (!projects.length) {
-        emptyElement.style['display'] = 'none';
-        sectionCardsElement.style['height'] = null;
-        sectionCardsElement.style['display'] = 'grid';
-        sectionCardsElement.style['align-items'] = null;
-        sectionCardsElement.style['justify-content'] = null;
-    }
+  if (!projects.length) {
+    emptyElement.style["display"] = "none";
+    sectionCardsElement.style["height"] = null;
+    sectionCardsElement.style["display"] = "grid";
+    sectionCardsElement.style["align-items"] = null;
+    sectionCardsElement.style["justify-content"] = null;
+  }
 
-    projects.push({ title, description });
+  projects.push({ title, description });
 
-    inputTitleElement.value = "";
-    inputDescriptionElement.value = "";
+  inputTitleElement.value = "";
+  inputDescriptionElement.value = "";
 
-    showForm();
-    listProjects();
+  showForm();
+  listProjects();
 }
 
-formElement.addEventListener('submit', onCreate);
+formElement.addEventListener("submit", onCreate);
+
+// gambiarra pra deletar os projetos quando clicar no X
+document.body.addEventListener("click", function (event) {
+  if (event.target.alt == "deletar") {
+    projects.splice(event.path[1].value, 1);
+    if (!projects.length) {
+      sectionCardsElement.style["height"] = "300px";
+      sectionCardsElement.style["display"] = "flex";
+      sectionCardsElement.style["align-items"] = "center";
+      sectionCardsElement.style["justify-content"] = "center";
+      emptyElement.style["display"] = "flex";
+    }
+    listProjects();
+  }
+});
